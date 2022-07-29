e28c8d4f2460304393c5faec1c81f254f2b81c63
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2022-07-29 18:55:55 +0200

    Add brent minimization, three shapes

--- src/shapes.rs
+++ src/shapes.rs
@@ -7,13 +7,38 @@ pub type Quaternion = na::UnitQuaternion<f64>;
 extern crate thiserror;
 use thiserror::Error;
 
+extern crate argmin;
+use argmin::prelude::*;
+use argmin::solver::brent::Brent;
+
 pub enum Name {
+    // 2
     Line,
-    Bent
+    Bent,
+    // 3
+    EquilateralTriangle,
+    VacantTetrahedron,
+    T,
+    // 4
+    // Tetrahedron,
+    // Square,
+    // Seesaw,
+    // TrigonalPyramid,
+    // 5
+    // SquarePyramid,
+    // TrigonalBipyramid,
+    // Pentagon,
+    // 6
+    // Octahedron,
+    // TrigonalPrism,
+    // PentagonalPyramid,
+    // Hexagon
 }
 
 use crate::permutation;
 pub type Permutation = permutation::Permutation;
+
+pub static ORIGIN_PLACEHOLDER: u8 = u8::MAX;
 
 pub struct Shape {
     pub name: Name,
@@ -52,7 +77,52 @@ lazy_static! {
         mirror: None
     };
 
-    pub static ref SHAPES: Vec<&'static Shape> = vec![&LINE, &BENT];
+    pub static ref EQUILATERAL_TRIANGLE: Shape = Shape {
+        name: Name::EquilateralTriangle,
+        size: 3,
+        repr: "equilateral triangle",
+        coordinates: Matrix3N::from_column_slice(&[
+            1.0, 0.0, 0.0,
+            -0.5, 0.866025, 0.0,
+            -0.5, -0.866025, 0.0
+        ]),
+        rotations: vec![
+            Permutation {sigma: vec![1, 2, 0]},
+            Permutation {sigma: vec![0, 2, 1]}
+        ],
+        tetrahedra: vec![],
+        mirror: None
+    };
+
+    pub static ref VACANT_TETRAHEDRON: Shape = Shape {
+        name: Name::VacantTetrahedron,
+        size: 3,
+        repr: "vacant tetrahedron",
+        coordinates: Matrix3N::from_column_slice(&[
+            0.0, -0.366501, 0.930418,
+            0.805765, -0.366501, -0.465209,
+            -0.805765, -0.366501, -0.465209
+        ]),
+        rotations: vec![Permutation {sigma: vec![2, 0, 1]}],
+        tetrahedra: vec![[ORIGIN_PLACEHOLDER, 0, 1, 2]],
+        mirror: Some(Permutation {sigma: vec![0, 2, 1]})
+    };
+
+    pub static ref TSHAPE: Shape = Shape {
+        name: Name::T,
+        size: 3,
+        repr: "T-shaped",
+        coordinates: Matrix3N::from_column_slice(&[
+            -1.0, -0.0, -0.0,
+            0.0, 1.0, 0.0,
+            1.0, 0.0, 0.0,
+        ]),
+        rotations: vec![Permutation {sigma: vec![2, 1, 0]}],
+        tetrahedra: vec![],
+        mirror: None
+    };
+
+    pub static ref SHAPES: Vec<&'static Shape> = vec![&LINE, &BENT, &EQUILATERAL_TRIANGLE, &VACANT_TETRAHEDRON, &TSHAPE];
 }
 
 pub fn shape_from_name(name: Name) -> &'static Shape {
@@ -142,6 +212,28 @@ pub fn apply_permutation(x: &Matrix3N, p: &Permutation) -> Matrix3N {
     Matrix3N::from_fn(x.ncols(), |i, j| x[(i, p[j] as usize)])
 }
 
+struct CoordinateScalingProblem<'a> {
+    pub cloud: &'a Matrix3N,
+    pub rotated_shape: &'a Matrix3N
+}
+
+impl ArgminOp for CoordinateScalingProblem<'_> {
+    type Param = f64;
+    type Output = f64;
+    type Hessian = ();
+    type Jacobian = ();
+    type Float = f64;
+
+    fn apply(&self, p: &Self::Param) -> Result<Self::Output, Error> {
+        let rmsd = (self.cloud.clone() - self.rotated_shape.scale(*p))
+            .column_iter()
+            .map(|col| col.norm_squared())
+            .sum();
+
+        Ok(rmsd)
+    }
+}
+
 pub fn polyhedron_similarity(x: &Matrix3N, s: Name) -> Result<f64, SimilarityError> {
     let shape = shape_from_name(s);
     let n = x.ncols();
@@ -161,18 +253,36 @@ pub fn polyhedron_similarity(x: &Matrix3N, s: Name) -> Result<f64, SimilarityErr
         let quaternion = fit_quaternion(&cloud, &permuted_shape);
         let permuted_shape = quaternion.to_rotation_matrix() * permuted_shape;
 
-        let rmsd = (cloud.clone() - permuted_shape).column_iter().map(|col| col.norm_squared()).sum();
+        let rmsd = (cloud.clone() - permuted_shape)
+            .column_iter()
+            .map(|col| col.norm_squared())
+            .sum();
+
         (p, rmsd, quaternion)
     };
 
-    let (best_permutation, _, best_rotation) = permutation
+    let (best_permutation, _, best_quaternion) = permutation
         .iter()
         .map(evaluate_permutation)
         .min_by(|(_, rmsd_a, _), (_, rmsd_b, _)| rmsd_a.partial_cmp(rmsd_b).expect("NaN in RMSDs"))
         .expect("Not a single permutation available to try");
 
     let permuted_shape = apply_permutation(&shape_coordinates, &best_permutation);
-    Ok(0.1)
+    let rotated_shape = best_quaternion.to_rotation_matrix() * permuted_shape;
+
+    let brent = Brent::new(0.5, 1.1, 1e-6);
+    let scaling_problem = CoordinateScalingProblem {
+        cloud: &cloud,
+        rotated_shape: &rotated_shape 
+    };
+    let result = Executor::new(scaling_problem, brent, 1.0)
+        .max_iters(100)
+        .run()
+        .unwrap();
+
+    println!("Result of Brent: {}", result);
+
+    Ok(result.state.best_cost)
 }
 
 #[cfg(test)]
